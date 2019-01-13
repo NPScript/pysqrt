bf0d2c1a7d724305968db39f13e25cf4aa089327
Nathan Reiner <nathanreiner@Nathans-MacBook-Air.local>
2019-01-13 15:34:29 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pysqrt',
+    version='0.1.1',
+    packages=['pysqrt'],
+    author="Nathan Reiner",
+    author_email="nathan.p.reiner@gmail.com",
+    long_description=open('README.txt').read(),
+    description="calculate the square root"
+)
