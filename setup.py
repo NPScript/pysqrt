6016a34abb404d4baf495ca81247dfc0b36674cb
Nathan Reiner <nathanreiner@Nathans-MacBook-Air.local>
2019-01-13 18:31:16 +0100

    setup update

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pysqrt',
-    version='0.1.1',
+    version='0.1.2',
     packages=['pysqrt'],
     author="Nathan Reiner",
     author_email="nathan.p.reiner@gmail.com",
     long_description=open('README.txt').read(),
-    description="calculate the square root"
+    description="calculate the square root",
+    python_requires='>3.0'
 )
